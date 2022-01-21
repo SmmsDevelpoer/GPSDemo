ed249e0f0e2b000783fb45c3f9236dfa2f888ce8
GavinWu <z7924723@gmail.com>
2022-01-21 14:45:08 +0800

    for test

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -15,15 +15,22 @@ export class AppService {
 
   public async turnOnUBlox() {
     await this.getUBloxComDevPath();
-    const path = '/dev/ttyS1';
-    this.uBlox = new SerialPort(path, { baudRate: 38400 }, (error: Error) => {
-      if (error) {
-        this.logger.error(
-          JSON.stringify(error),
-          `Com port open failed, device path "${path}"`,
-        );
-      }
-    });
+    let path = '/dev/ttyS';
+    for (let index = 0; index < 4; index++) {
+      path += String(index);
+      this.uBlox = new SerialPort(
+        path,
+        { baudRate: 38400, dataBits: 8, stopBits: 1, parity: 'none' },
+        (error: Error) => {
+          if (error) {
+            this.logger.error(
+              JSON.stringify(error),
+              `Com port open failed, device path "${path}"`,
+            );
+          }
+        },
+      );
+    }
     //
     // this.uBlox.on('open', (error) => {
     //   console.log('serial port open success.');
